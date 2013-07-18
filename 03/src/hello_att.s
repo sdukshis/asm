f24ce32d1d54d024e3d5b9f9e3b602dfbfe27c49
Pavel Filonov <pfilonov@ptsecurity.ru>
2013-07-18 10:59:32 +0400

    Lec 3 x86 and nasm intro

---
+++ 03/src/hello_att.s
@@ -0,0 +1,17 @@
+.globl _start
+
+.data
+hello: .string "Hello world\n"
+
+.text
+_start:
+    movl $4, %eax
+    movl $1, %ebx
+    movl $hello, %ecx
+    movl $13, %edx
+    int $0x80
+
+    movl $1, %eax
+    movl $0, %ebx
+    int $0x80
+
